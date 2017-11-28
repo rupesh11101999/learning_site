635b6415462726845551788e7b1ca56968b5307d
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-11-27 23:03:59 -0500

    Added dynamic URL namespace-matching to layout links

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Course
+from .models import Course, Step
 
 
 def course_list(request):
@@ -18,4 +18,17 @@ def course_detail(request, pk):
         request,
         'courses/course_detail.html',
         {'course':course}
+    )
+
+
+def step_detail(request, course_pk, step_pk):
+    step = get_object_or_404(
+        Step,
+        course_id=course_pk,
+        pk=step_pk
+    )
+    return render(
+        request,
+        'courses/step_detail.html',
+        {'step':step}
     )
