b64480da6d2b0bd7579a09a96adeacf16e0686ee
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-11-27 21:57:26 -0500

    added steps to courses

--- learning_site/courses/models.py
+++ learning_site/courses/models.py
@@ -9,3 +9,17 @@ class Course(models.Model):
 
     def __str__(self):
         return str(self.title)
+
+
+class Step(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    order = models.IntegerField(default=0)
+    course = models.ForeignKey(Course)
+
+    class Meta:
+        ordering = ['order',]
+
+
+    def __str__(self):
+        return self.title
--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -1,11 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Course
+
 
 def course_list(request):
     courses = Course.objects.all()
     return render(
         request,
         'courses/course_list.html', # omit 'template/', it is implied
-        {'courses': courses})
+        {'courses': courses}
+    )
+
+
+def course_detail(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    return render(
+        request,
+        'courses/course_detail.html',
+        {'course':course}
+    )
